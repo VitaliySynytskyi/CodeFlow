8a6eaa944cbaebb5c2c46f6db6d520497556e073
vitaliy <howery1337@gmail.com>
2023-03-15 20:49:55 +0200

    Initial commit

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/question')
+def index1():
+    return render_template('question.html')
 
 
 @app.route("/members")
