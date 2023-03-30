4acfa0f13389186bddf5129d35e41ac4e251b95a
vitaliy <howery1337@gmail.com>
2023-03-30 22:00:03 +0300

    New home and about page

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
 from datetime import datetime
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-class MyTable(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-
-@app.route('/table')
-def index1():
-    results = MyTable.query.all()
-    return str(results)
 
 
 @app.route('/')
@@ -24,22 +10,28 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    print('Request for about page received')
+    return render_template('about.html')
+
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    name = request.form.get('name')
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#     name = request.form.get('name')
 
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return render_template('hello.html', name=name)
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return redirect(url_for('index'))
+#     if name:
+#         print('Request for hello page received with name=%s' % name)
+#         return render_template('hello.html', name=name)
+#     else:
+#         print('Request for hello page received with no name or blank name -- redirecting')
+#         return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
