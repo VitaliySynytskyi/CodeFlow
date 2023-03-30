a97fb1c9d32eaac7ab6884e29e8d3d053768e47f
howery <howery1337@gmail.com>
2023-03-30 10:12:58 +0300

    add db

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from datetime import datetime
+import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class MyTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+@app.route('/table')
+def index1():
+    results = MyTable.query.all()
+    return str(results)
 
 
 @app.route('/')
