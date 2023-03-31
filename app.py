3a8ea53878b3eb2cbdf3e4e2f2710c8bc571d2bc
vitaliy <howery1337@gmail.com>
2023-03-31 15:43:55 +0300

    req

--- app.py
+++ app.py
@@ -10,36 +10,36 @@ from config import Config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = Config.SECRET_KEY
-app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = Config.APPINSIGHTS_INSTRUMENTATIONKEY
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
-swagger = Swagger(app)
-appinsights = AppInsights(app)
-logging.basicConfig(filename='app.log', level=logging.DEBUG,
-                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-logger = logging.getLogger(__name__)
+# app.config['SECRET_KEY'] = Config.SECRET_KEY
+# app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = Config.APPINSIGHTS_INSTRUMENTATIONKEY
+# app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+# swagger = Swagger(app)
+# appinsights = AppInsights(app)
+# logging.basicConfig(filename='app.log', level=logging.DEBUG,
+#                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+# logger = logging.getLogger(__name__)
 
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    logger.debug('Request for login page received')
-    form = LoginForm()
-    if form.validate_on_submit():
-        # тут буде логіка для перевірки даних з форми
-        # та входу користувача на сайт
-        flash('You have been logged in!', 'success')
-        return redirect(url_for('home'))
-    return render_template('login.html', title='Login', form=form)
+# @app.route("/login", methods=['GET', 'POST'])
+# def login():
+#     logger.debug('Request for login page received')
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         # тут буде логіка для перевірки даних з форми
+#         # та входу користувача на сайт
+#         flash('You have been logged in!', 'success')
+#         return redirect(url_for('home'))
+#     return render_template('login.html', title='Login', form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    logger.debug('Request for register page received')
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     logger.debug('Request for register page received')
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('home'))
+#     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/hello')
@@ -51,21 +51,21 @@ def hello():
       200:
         description: A string indicating a greeting.
     """
-    app.logger.info('Hello, World!')
-    logger.debug('Entering hello string')
+    # app.logger.info('Hello, World!')
+    # logger.debug('Entering hello string')
     return 'Hello, world!'
 
 
 @app.route('/')
 @app.route("/home")
 def home():
-    logger.debug('Request for index page received')
+    # logger.debug('Request for index page received')
     return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    logger.debug('Request for about page received')
+    # logger.debug('Request for about page received')
     return render_template('about.html')
 
 
