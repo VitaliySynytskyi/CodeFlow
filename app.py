23ad1cf6b7c8785a52d784527ee0f3d7872ea880
vitaliy <howery1337@gmail.com>
2023-03-31 13:25:01 +0300

    add secrets

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from applicationinsights.flask.ext import AppInsights
 from flasgger import Swagger
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from forms import LoginForm, RegistrationForm
@@ -7,14 +9,21 @@ from models import User
 from config import Config
 from database import db
 
+
 app = Flask(__name__)
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = Config.APPINSIGHTS_INSTRUMENTATIONKEY
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 swagger = Swagger(app)
+appinsights = AppInsights(app)
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logger = logging.getLogger(__name__)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    logger.debug('Request for login page received')
     form = LoginForm()
     if form.validate_on_submit():
         # тут буде логіка для перевірки даних з форми
@@ -26,6 +35,7 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    logger.debug('Request for register page received')
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
@@ -42,19 +52,21 @@ def hello():
       200:
         description: A string indicating a greeting.
     """
+    app.logger.info('Hello, World!')
+    logger.debug('Entering hello string')
     return 'Hello, world!'
 
 
 @app.route('/')
 @app.route("/home")
 def home():
-    print('Request for index page received')
+    logger.debug('Request for index page received')
     return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    print('Request for about page received')
+    logger.debug('Request for about page received')
     return render_template('about.html')
 
 
@@ -62,32 +74,6 @@ def about():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data,
-                    email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and user.password == form.password.data:
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    APPINSIGHTS_INSTRUMENTATIONKEY = os.getenv(
+        'APPINSIGHTS_INSTRUMENTATIONKEY')
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
