3e4ddaac0c5fd3f51bacbb3f87f5c989211a7807
vitaliy <howery1337@gmail.com>
2023-05-04 15:44:50 +0300

    all ok

--- app.py
+++ app.py
@@ -83,12 +83,14 @@ from models import User
 from forms import login_form,register_form
 import requests
 
+#картинки про котів
 api_key = 'live_nh2nBNg7o0ezAaPFStcGG8LWXhJSSnDdezIO6YnqDlu4DxKSDjwyXY0hgnOLOe4k'
 def get_random_cat():
     response = requests.get(f'https://api.thecatapi.com/v1/images/search?api_key={api_key}')
     cat_data = response.json()
     return cat_data[0]['url']
 
+#факти про котів
 def get_random_cat_fact():
     response = requests.get(f'https://catfact.ninja/fact')
     cat_data = response.json()
